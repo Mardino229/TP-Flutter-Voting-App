6032d2570eb22bceccfa5b418eec911cda95ceb7
Brandondgb <brandondegbe95@gmail.com>
2025-01-24 08:38:22 +0100

    update

--- Optivote/lib/main.dart
+++ Optivote/lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:optivote/Ajouter_vote/dashboard_vote.dart';
-import 'package:optivote/Ajouter_vote/lol.dart';
 import 'package:optivote/Ajouter_vote/voir_tous_les_votes.dart';
 import 'package:optivote/pages/connexion.dart';
 import 'package:optivote/pages/inscription.dart';
@@ -58,10 +56,6 @@ final _router = GoRouter(
     GoRoute(
       path: '/see_all_vote',
       builder: (context, state) => SeeAllVotes(),
-    ),
-    GoRoute(
-      path: '/home',
-      builder: (context, state) => HomeScreen(),
     ),
     //liens vers les pages user_pages
     GoRoute(
